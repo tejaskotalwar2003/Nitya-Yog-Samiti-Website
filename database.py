0b14f8791ad6d7574f59a4b781a95502c0a9772f
Tejas Kotalwar <117026179+tejaskotalwar2003@users.noreply.github.com>
2026-03-17 19:50:38 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgresql@localhost/yogsamiti"
+DATABASE_URL = "postgresql://yogsamiti_user:mb3li9c0AUaeZQXdmwDaUKoTSjw2KBFE@dpg-d6rqp9ggjchc73bf371g-a/yogsamiti"
 
 engine = create_engine(DATABASE_URL)
 
