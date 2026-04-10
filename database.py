8e25ac865b3d78c9c129a779d188056bace50f51
Tejas Kotalwar <117026179+tejaskotalwar2003@users.noreply.github.com>
2026-04-10 17:05:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,19 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise Exception("❌ DATABASE_URL not set")
+    raise Exception("DATABASE_URL not set ❌")
 
-# Render PostgreSQL fix (important)
+# Fix postgres URL (Render)
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
+
+# 🔥 THIS LINE WAS MISSING
+Base = declarative_base()
