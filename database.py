521f64475ca5e5a2a7d0c514b475f9a2dd02144f
Tejas Kotalwar <117026179+tejaskotalwar2003@users.noreply.github.com>
2026-04-10 17:00:17 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # 🔥 get from Render env
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"sslmode": "require"}  # 🔥 VERY IMPORTANT
-)
+if not DATABASE_URL:
+    raise Exception("❌ DATABASE_URL not set")
+
+# Render PostgreSQL fix (important)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
